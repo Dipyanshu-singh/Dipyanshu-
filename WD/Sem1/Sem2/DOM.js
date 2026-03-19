e73f4b91d3823891d1d87249ffa63d57208fe9f1
Shred Sharma <ntic.shred@gmail.com>
2026-03-19 11:35:19 +0530

    s1ex1

--- WD/Sem1/Sem2/DOM.js
+++
@@ -1,9 +0,0 @@
-// Select elements
-let title = document.getElementById("title");
-let button = document.getElementById("btn");
-
-// Add event
-button.addEventListener("click", function() {
-    title.textContent = "Button Clicked!";
-    title.style.color = "blue";
-});
---
+++ WD/Sem2/DOM.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("btn");
+let newheading=document.getElementById("tittle")
+ newheading.textContent+"DOM manipulation in JS";
+ newheading.style.color="red";
+ newheading.style.fontSize="50px;"
+ let paragraph=document.getElementsByClassName("")
+ button.addEventListener("click", function() {
+    title.textContent = "Button Clicked!";
+    title.style.color = "blue";
+});
