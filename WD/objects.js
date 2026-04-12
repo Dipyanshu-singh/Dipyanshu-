9a6536a614f4a69cc4b3841524a0f30c231499b6
Shred Sharma <ntic.shred@gmail.com>
2026-04-13 00:53:09 +0530

    ..

---
+++ WD/objects.js
@@ -0,0 +1,28 @@
+let person ={
+    name: "Shred",
+    age: "30",
+    city:"New york"
+};
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+
+let numbers = [1,2,3,4,5];
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+
+
+let mixedArray = [1,"Hello",true,{name:"Alice"},[1,2,3]];
+console.log(mixedArray[0]);
+console.log(mixedArray[1]);
+console.log(mixedArray[2]);
+console.log(mixedArray[3]);
+console.log(mixedArray[4]);
+
+let newarray = [1,3,4,5];
+let squaredArray=newarray.map(num => num * num);
+console.log(squaredArray);
+
+
+
--- c++/selectionsort.cpp
+++ c++/selectionsort.cpp
@@ -9,7 +9,7 @@ int main() {
         int min_i = i;
 
         for(int j = i + 1; j < n; j++) {
-            if(arr[j] < arr[minIndex]) {
+            if(arr[j] < arr[min_i]) {
                 min_i = j;
             }
         }
