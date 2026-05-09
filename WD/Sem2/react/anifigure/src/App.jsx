1f2c31ad13022d5737659d8b6f5a0f2fad7323b8
Shred Sharma <ntic.shred@gmail.com>
2026-05-09 12:56:20 +0530

    stucard

--- WD/Sem2/react/anifigure/src/App.jsx
+++ WD/Sem2/react/anifigure/src/App.jsx
@@ -1,10 +1,21 @@
+import StudentCard from './StudentCard'
+
+import  './StudentCard.css'
+
 function App(){
+
     return(
-        <div className="App">
+        <div className="card">
             <h1>Hello World</h1>
             <p>Thisis my first React app.</p>
             <CSE12></CSE12>
+            <h1>Student Card</h1>
+            <StudentCard name="Shred" age="101" grade="lallantop"></StudentCard>
+            <StudentCard name="Tiwari" age="69" grade="zhakkas"></StudentCard>
+            <StudentCard name="PeriPeri" age="18" grade="Aaaaag"></StudentCard>
+
         </div>
+        
     );
 }
 function CSE12(){
@@ -16,4 +27,5 @@ function CSE12(){
         </div>
     );
 }
+
 export default App;
---
+++ WD/Sem2/react/anifigure/src/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard({name,age,grade}){
+    return(
+        <div>
+            <h2>Name:{name}</h2>
+            <p>Age:{age}</p>
+            <p>Grade:{grade}</p>
+        </div>
+    );
+
+}
+export default StudentCard;
